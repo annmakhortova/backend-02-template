04691edd3e15ebb1b47f63ff5465e5c44b37b5d4
annmakhortova <an-chudi@email.com>
2024-02-06 14:19:56 +0300

    question

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const http = require("http");
 const getUsers = require("./modules/users");
 
-const host = "http://127.0.0.1";
-const port = 3003;
-
 const server = http.createServer((request, response) => {
   // Написать обработчик запроса:
   // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
@@ -16,47 +13,39 @@ const server = http.createServer((request, response) => {
   const url = new URL(request.url, ipAddress);
   const userName = url.searchParams.get("hello");
 
-  if (userName) {
-    response.statusCode = 200;
+  if (request.url === `/?hello=<name>`) {
+    response.status = 200;
     response.statusMessage = "ok";
     response.header = "Content-Type: text/plain";
-    response.write(`Hello, ${userName}.`);
+    response.write(`Hello, <name>`);
     response.end();
+
     return;
   }
 
-  if (request.url === "/?hello") {
-    response.statusCode = 400;
-    response.statusMessage = "Bad Request";
-    response.header = "Content-Type: text/plain";
-    response.write(`Enter a name`);
-    response.end();
-    return;
-  }
-
-  if (request.url === `/?users`) {
-    response.statusCode = 200;
+  if (request.url === `?users`) {
+    response.status = 200;
     response.statusMessage = "ok";
     response.header = "Content-Type: application/json";
     response.write(getUsers());
     response.end();
+
     return;
   }
 
-  if (request.url === "/") {
-    response.statusCode = 200;
+  if (!request.url) {
+    response.status = 200;
     response.statusMessage = "ok";
     response.header = "Content-Type: text/plain";
     response.write(`Hello, World!`);
     response.end();
+
     return;
   }
 
-  response.statusCode = 500;
-  response.statusMessage = "Internal Server Error";
-  response.end();
+  response.status = 500;
 });
 
-server.listen(port, () =>
-  console.log(`Сервер запущен по адресу ${host + ":" + port}`)
+server.listen(3003, () =>
+  console.log(`Сервер запущен по адресу http://127.0.0.1:3003`)
 );
